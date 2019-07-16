ff5220b83b38bc833f43815b90ec99195c34509f
j m <jm@js-MacBook-Pro.local>
2019-07-16 12:52:45 +0200

    moving forward

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,13 +9,18 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
+hbs.registerPartials(__dirname + '/views/partials')
 
 
 app.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index.hbs');
 });
-
+app.get('/layout', (req, res, next) => {
+  res.render('layout.hbs');
+})
+app.get('/beers', (req, res, next ) => {
+  res.render('beers.hbs');
+})
 
 
 app.listen(3000);
